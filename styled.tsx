7560d7626e3a870b04b96f6f4133dc1a403603b2
Alexander Alemayhu <alexander@alemayhu.com>
2022-10-30 21:06:17 +0100

    chore: add styled-components

---
+++ styled.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: "#fff";
+  align-items: "center";
+  justify-content: "center";
+`;
+
+export const Title = styled.Text`
+  color: #5397f5;
+  text-align: center;
+  font-size: 3rem;
+`;
