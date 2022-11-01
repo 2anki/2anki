0c2d61a6c7d850bbaaa5999db1d516d3787287c3
Alexander Alemayhu <alexander@alemayhu.com>
2022-11-01 18:44:36 +0100

    style: remove unused import

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button } from 'react-native';
 import { Container, PageHeader, Title } from './shared/styled';
 
 export default function App() {
